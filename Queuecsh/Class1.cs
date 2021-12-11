edb76b019b5a928242c5a19a7507f36bc9e8c6d5
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2021-12-11 07:28:30 +0530

    Queue Enqueue

---
+++ Queuecsh/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queuecsh
+{
+    public class Class1
+    {
+        public class Queue
+        {
+            LinkedList<int> queue;
+            public Queue()
+            {
+                queue = new LinkedList<int>();
+            }
+            public void Enqueue(int val)
+            {
+                queue.AddLast(val);
+            }
+            public int Dequeue()
+            {
+                if (queue.Count == 0)return 0;
+                int tmp = queue.First.Value;
+                queue.RemoveFirst();
+                return tmp;
+            }
+            public int Peek()
+            {
+                if (queue.Count<=0)return 0;
+                int tmp = queue.First.Value;
+                return tmp;
+            }
+            public int GetCount()
+            {
+                return queue.Count;
+            }
+
+        }
+
+    }
+}
+    
+
--- Queuecsh/Program.cs
+++ Queuecsh/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, to Queue programe!");
+//Console.WriteLine("Hello, to Queue programe!");
+using Queuecsh;
+Queue<int> qu = new Queue<int>();
+qu.Enqueue(1);
+qu.Enqueue(2);
+qu.Enqueue(3);
+while (qu.Count() > 0)
+    Console.WriteLine(qu.Dequeue());
